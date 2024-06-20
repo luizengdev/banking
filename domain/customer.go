0fc2b4d83e65b6a0a37a3490e222009395cef23f
luizengdev <luizengdev@gmail.com>
2024-06-20 19:19:22 -0300

    feat: add func ToDto converts the customer to CustomerResponse

--- domain/customer.go
+++ domain/customer.go
@@ -1,6 +1,9 @@
 package domain
 
-import "github.com/luizengdev/banking/errs"
+import (
+	"github.com/luizengdev/banking/dto"
+	"github.com/luizengdev/banking/errs"
+)
 
 // Customer represents a customer with personal and contact information.
 type Customer struct {
@@ -10,6 +13,29 @@ type Customer struct {
 	Zipcode     string
 	DateOfBirth string `db:"date_of_birth"`
 	Status      string
+}
+
+// statusAsText returns the customer status as text.
+func (c Customer) statusAsText() string {
+	statusAsText := "active"
+
+	if c.Status == "0" {
+		statusAsText = "inactive"
+	}
+	return statusAsText
+}
+
+// ToDto converts the customer to a CustomerResponse.
+func (c Customer) ToDto() dto.CustomerResponse {
+
+	return dto.CustomerResponse{
+		Id:          c.Id,
+		Name:        c.Name,
+		City:        c.City,
+		Zipcode:     c.Zipcode,
+		DateOfBirth: c.DateOfBirth,
+		Status:      c.statusAsText(),
+	}
 }
 
 // CustomerRepository defines the operations available for working with customer data.
