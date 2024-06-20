93f3faa9a1185ea791e23bccea5d555d86a453e6
luizengdev <luizengdev@gmail.com>
2024-06-20 19:21:22 -0300

    refactor: add dto.CustomerResponse to logic

--- service/customerService.go
+++ service/customerService.go
@@ -2,13 +2,14 @@ package service
 
 import (
 	"github.com/luizengdev/banking/domain"
+	"github.com/luizengdev/banking/dto"
 	"github.com/luizengdev/banking/errs"
 )
 
 // CustomerService defines the service operations available to customers.
 type CustomerService interface {
-	GetAllCustomers(string) ([]domain.Customer, *errs.AppError)
-	GetCustomer(string) (*domain.Customer, *errs.AppError)
+	GetAllCustomers(string) (*[]dto.CustomerResponse, *errs.AppError)
+	GetCustomer(string) (*dto.CustomerResponse, *errs.AppError)
 }
 
 // DefaultCustomerService is the default implementation of CustomerService.
@@ -16,21 +17,31 @@ type DefaultCustomerService struct {
 	repo domain.CustomerRepository
 }
 
-// GetAllCustomers fetches all customers using the associated repository.
-func (s DefaultCustomerService) GetAllCustomers(status string) ([]domain.Customer, *errs.AppError) {
-	if status == "active" {
-		status = "1"
-	} else if status == "inactive" {
-		status = "0"
-	} else {
-		status = ""
+// GetAllCustomers fetches all customers using the associated repository and returns a CustomerResponse slice.
+func (s DefaultCustomerService) GetAllCustomers(status string) (*[]dto.CustomerResponse, *errs.AppError) {
+	customers, err := s.repo.FindAll(status)
+	if err != nil {
+		return nil, err
 	}
-	return s.repo.FindAll(status)
+
+	var responses []dto.CustomerResponse
+	for _, c := range customers {
+		responses = append(responses, c.ToDto())
+	}
+
+	return &responses, nil
 }
 
-// GetCustomer fetches a customer using the associated repository.
-func (s DefaultCustomerService) GetCustomer(id string) (*domain.Customer, *errs.AppError) {
-	return s.repo.FindById(id)
+// GetCustomer fetches a customer using the associated repository and returns a CustomerResponse.
+func (s DefaultCustomerService) GetCustomer(id string) (*dto.CustomerResponse, *errs.AppError) {
+	c, err := s.repo.FindById(id)
+	if err != nil {
+		return nil, err
+	}
+
+	response := c.ToDto()
+
+	return &response, nil
 }
 
 // NewCustomerService creates a new instance of the DefaultCustomerService with the provided repository.
